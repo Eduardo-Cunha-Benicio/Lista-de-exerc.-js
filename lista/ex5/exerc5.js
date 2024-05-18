aa8211cf2c77624eef0026573dea263b614e7441
Eduardo-Cunha-Benicio <164964259+Eduardo-Cunha-Benicio@users.noreply.github.com>
2024-05-17 23:11:11 -0300

    Delete lista directory

--- lista/ex5/exerc5.js
+++
@@ -1,9 +0,0 @@
-let valor1 = document.querySelector("#valor1");
-let valor2 = document.querySelector("#valor2");
-let btMaior = document.querySelector("#btMaior");
-
-function retornarMaior(){
-    let vlr1 = Number (valor1.value);
-    let vlr2 = Number (valor2.value);
-    
-}
